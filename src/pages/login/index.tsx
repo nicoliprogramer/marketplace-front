4098fcc715b33660dfb6ade5215a141902cc633c
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-08 15:15:37 -0300

    login page and form management

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import { Container, Button } from "@mui/material";
+import { Container, Button, Grid, Paper, Box, Typography, TextField, Stack } from "@mui/material";
+
+type LoginType = { 
+    username: string,
+    password: string
+}
 
 export const LoginPage: React.FC<{}> = () => {
+    const [loginData, setLoginData] = React.useState<LoginType>({
+        username: "",
+        password: ""
+    })
+
+    const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginData({...loginData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        console.log(loginData)        
+    }
+
     return (
-        <Container sx={{mt: 9}} maxWidth="xl">
-            <Button fullWidth variant="contained">
-                Estamos en login
-            </Button>
-      </Container>
+        <Container maxWidth="sm">
+            <Grid container direction="column" alignItems="center" justifyContent="center" sx={{minHeight: "100vh"}}>
+                <Grid item>
+                    <Paper sx={{padding:"1.2em", borderRadius:"0.5em"}}>
+                        <Typography sx={{mt:1, mb:1}} variant="h4">Iniciar Sesión</Typography>
+                        <Box component="form" onSubmit={handleSubmit}>
+                            <Stack spacing={2}>
+                                <TextField name="username" margin="normal" type="text" fullWidth label="email" sx={{mt:2, mb:1.5}} required onChange={dataLogin}/>
+                                <TextField name="password" margin="normal" type="password" fullWidth label="password"sx={{mt:1.5, mb:1.5}} required onChange={dataLogin}/>
+                            </Stack>
+                            <Button fullWidth type="submit" variant="contained" sx={{mt:1.5, mb:3}}>Iniciar Sesión</Button>
+                        </Box>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
