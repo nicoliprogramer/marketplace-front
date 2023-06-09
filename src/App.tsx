21b360733af9254a5c257c2dfdec075949ba7f23
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-08 23:35:12 -0300

    creating notifications with context

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter} from "react-router-dom"
 import { AppRouter } from './Router';
+import { NotificationProvider } from './context/notification.context';
 function App() {
   return (
-    <BrowserRouter>
-        <AppRouter/>
-    </BrowserRouter>
+    <NotificationProvider>
+      <BrowserRouter>
+          <AppRouter/>
+      </BrowserRouter>
+    </NotificationProvider>
+    
   );
 }
 
