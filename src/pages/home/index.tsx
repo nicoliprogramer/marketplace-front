67c0b11c76acb1c0e9e5bcc1f04b7025a954cb76
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-09 22:43:15 -0300

    connecting an api with axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { Container, Button } from "@mui/material";
 import { useNotification } from "../../context/notification.context";
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
     
+    React.useEffect(() => {
+            characters.getById({id: 1}).then((r)=> {
+                console.log(r.data);
+                
+            }).catch((e)=> {
+                console.error(e)
+            })
+        },[])
+
     return (
         <Container maxWidth="xl">
             <HeaderComponent title="Hola mundillo" description="Estas en un buen lugar" element={<Button fullWidth variant="contained">PRESS</Button>}/>
